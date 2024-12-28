8fa9db472fb4962c2caa0c510060968753a0c017
Thomas Adamcik <thomas@adamcik.no>
2024-12-28 11:57:58 +0100

    Initial commit for python-lspconfig.nvim
    This is being split out from my nvim config as I suspect it might be useful for
    others. The idea here is to auto configure `lspconfig` for `pyright`/`basedpyright`
    based on a helper for finding the python path based on the root directory the
    LSP found.
    
    Users must opt in to having things auto configured, just set `opts = {}`.
    Additionally there is a `python_path(dir)` helper in case you want to configure
    things yourself.
    
    Changing the active `VIRTUAL_ENV` is not in scope (at least for now). But
    thanks to the helper you could always wire this up with the right config.

---
+++ README.md
@@ -0,0 +1,57 @@
+# python-lspconfig
+
+This plugin can automatically configure `lspconfig` to use the right
+`pythonPath` for `pyright` and `basedpyright`. Additionally you can opt to used
+`python_path(dir)` if you just want a helper to determine the python interpreter
+that should be used for this env.
+
+Note that this plugin does not try and activate virtual envs, it just finds them
+and configures the LSP. For my own use this seems to work well for the
+monorepo setup we have at work.
+
+If using lazy just add the following to get things autoatically configured.
+`opts` can also contain a `on_new_config` key, which can be set to a
+lsp+function to use as the `on_new_config` hook.
+
+```lua
+{ 'adamcik/python-lspconfig', opts = {} }
+```
+
+Note that this depends on [plenary](https://github.com/nvim-lua/plenary.nvim) to
+make handling files and paths way nicer. If you are using lazy this handled.
+
+If you don't want to use the auto configuration, just omit opts and do something
+like (just adapted to your config flavour / helpers).
+
+```lua
+local lspconfig = require('lspconfig')
+
+lspconfig.basedpyright.setup({
+  on_new_config = function(config, root)
+    local python_path = require('python-lspconfig').python_path(root)
+    if python_path and python_path ~= '' then
+      config.settings.python.pythonPath = python_path
+    end
+  end
+})
+```
+
+## Supported tools
+
+- Explicitly set `VIRTUAL_ENV` or `CONDA_PREFIX`
+- `uv`, just searching for `.venv` in nearest parent.
+- `poetry` asking for the python executable.
+- `pipenv` asking for the python executable.
+- `pdm` asking for the python executable.
+- Fallback, just looking for a venv in the directory.
+
+I've only really tested `poetry` and the `uv` search variant. Hopefully the rest
+work as expected.
+
+## Inspiration
+
+The following config/plugins helped with showing how to determine how to make
+all this work:
+
+- [github.com/jglasovic/venv-lsp.nvim](https://github.com/jglasovic/venv-lsp.nvim)
+- [github.com/younger-1/nvim](https://github.com/younger-1/nvim/blob/6088053681928c1ab342f857622039f5d1db9498/lua/young/lang/python.lua)
---
+++ lua/python-lspconfig/init.lua
@@ -0,0 +1,102 @@
+local Path = require("plenary.path")
+local scan = require("plenary.scandir")
+local lspconfig = require("lspconfig")
+
+local python_path_mem = {}
+
+local function exec(opts)
+    local result = vim.system(opts.cmd, { text = true, cwd = opts.cwd }):wait()
+    return result.code == 0 and vim.fn.trim(result.stdout) or nil
+end
+
+local function path_to_python_path(path)
+    return (path / "bin" / "python").filename
+end
+
+local function determine_python(root)
+    if vim.env.VIRTUAL_ENV and vim.env.VIRTUAL_ENV ~= "" then
+        return path_to_python_path(Path:new(vim.env.VIRTUAL_ENV))
+    end
+
+    if vim.env.CONDA_PREFIX and vim.env.CONDA_PREFIX ~= "" then
+        return path_to_python_path(Path:new(vim.env.CONDA_PREFIX))
+    end
+
+    if (root / "uv.lock"):exists() then
+        -- uv just searches for nearest .venv
+        for _, parent in pairs(root:parents()) do
+            if (parent / ".venv"):exists() then
+                return path_to_python_path(parent / ".venv")
+            end
+        end
+    end
+
+    if (root / "poetry.lock"):exists() then
+        local result = exec({ cmd = { "poetry", "env", "info", "--executable" }, cwd = root.filename })
+        if result then
+            return result
+        end
+    end
+
+    if (root / "Pipfile"):exists() then
+        local result = exec({ cmd = { "pipenv", "--py" }, cwd = root.filename })
+        if result then
+            return result
+        end
+    end
+
+    if (root / "pdm.lock"):exists() then
+        local result = exec({ cmd = { "pdm", "info", "--python" }, cwd = root.filename })
+        if result then
+            return result
+        end
+    end
+
+    -- Fallback to any venv we can directly under root
+    local opts = { hidden = true, depth = 2, search_pattern = "pyvenv.cfg" }
+    for _, path in pairs(scan.scan_dir(root.filename, opts)) do
+        return path_to_python_path(Path:new(path):parent())
+    end
+    return nil
+end
+
+local M = {}
+
+M.python_path = function(dir)
+    if not python_path_mem[dir] then
+        local python = determine_python(Path:new(dir))
+        if python and python ~= "" then
+            python_path_mem[dir] = python
+        end
+    end
+    return python_path_mem[dir]
+end
+
+local function determine_and_set_python_path(config, root)
+    local python = M.python_path(root)
+    if python and python ~= "" then
+        -- TODO: See if we need pep582 support with uv?
+        -- config.settings.python.analysis.extraPaths = ...
+        config.settings.python.pythonPath = python
+    end
+end
+
+-- Default on config mapping for (based)pyright
+M.on_new_config = {
+    pyright = determine_and_set_python_path,
+    basedpyright = determine_and_set_python_path,
+}
+
+M.setup = function(opts)
+    -- Merge whatever config user has provivded for `on_new_config`
+    local on_new_config = vim.tbl_extend("force", M.on_new_config, opts.on_new_config or {})
+
+    -- Instal setup and config hooks for the configured tools:
+    lspconfig.util.on_setup = lspconfig.util.add_hook_after(lspconfig.util.on_setup, function(config)
+        if on_new_config[config.name] then
+            config.on_new_config = lspconfig.util.add_hook_after(config.on_new_config, on_new_config[config.name])
+        end
+    end)
+end
+
+return M
